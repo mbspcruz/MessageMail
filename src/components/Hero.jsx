4dfc9f6a25e5bd90616271fb4f14e5bd48e141db
Mariana  Cruz <mda-cruz@Marianas-Air-2.Home>
2023-03-28 10:59:50 +0100

    Done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
   return (
     <div
       id="home"
-      className="flex mt-40 flex-col md:px-32 px-8 w-full justify-between lg:items-start items-center text-center md:text-left md:flex-row font-nunito"
+      className="flex mt-40 flex-col md:px-32 px-8 w-full md:justify-between justify-center lg:items-start items-center text-center md:text-left md:flex-row font-nunito"
     >
       <div className="md:w-1/2 lg:pr-24 mt-20">
         <h1 className="lg:text-7xl text-5xl font-bold text-primary-25">
@@ -15,7 +15,7 @@ export default function Hero() {
           We offer you a central location to manage all of your social media
           account's direct messages!
         </h2>
-        <div className=" mt-8 w-1/2 mb-20">
+        <div className="mt-8 w-full mb-20 flex md:block justify-center">
           <ButtonForm />
         </div>
       </div>
